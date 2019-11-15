5a25d90b4f157a63129d8bb0ff897367d52fff35
piter123 <pietro.odaglia@gmail.com>
2019-11-15 23:06:23 +0100

    done

--- texas/boundary.py
+++ texas/boundary.py
@@ -58,61 +58,60 @@ def check_size(points, max_x=0.0, max_y=0.0):
 
 def get_distance(point0, point1):
     return math.sqrt(
-        math.pow(point0[0] - point1[0], 2.0) + math.pow(point0[1] - point1[1], 2.0) + math.pow(point0[2] - point1[2],
-                                                                                               2.0))
+        math.pow(point0[0] - point1[0], 2.0) + math.pow(point0[1] - point1[1], 2.0) +
+        math.pow(point0[2] - point1[2], 2.0))
 
 
-def compute_edge(orientation):
-
-    # ==============================================================================
-    # Vertices on boundary
-    # ==============================================================================
-
-    boundary = list(cablenet.vertices_where({'constraint': orientation}))
-    ordered = list(cablenet.vertices_on_boundary(ordered=True))
-    boundary[:] = [key for key in ordered if key in boundary]
-
-    # ==============================================================================
-    # Boundary plane
-    # ==============================================================================
-
+def get_boundary_plane(boundary):
     a = cablenet.vertex_coordinates(boundary[0])
     b = cablenet.vertex_coordinates(boundary[-1])
 
-    x_axis = subtract_vectors(b,a)
+    x_axis = subtract_vectors(b, a)
     y_axis = [0, 0, 1.0]
     z_axis = cross_vectors(x_axis, y_axis)
 
     x_axis = cross_vectors(y_axis, z_axis)
 
-    frame = Frame(a, x_axis, y_axis)
-    point = add_vectors(frame.point, scale_vector(frame.zaxis, OFFSET))
-    normal = frame.zaxis
-    plane = point, normal
+    frame_0 = Frame(a, x_axis, y_axis)
+    point = add_vectors(frame_0.point, scale_vector(frame_0.zaxis, OFFSET))
+    frame_1 = Frame(point, x_axis, y_axis)
+    return frame_0, frame_1
 
-    # ==============================================================================
-    # Intersections
-    # ==============================================================================
 
+def intersect_residual(boundary, frame):
     intersections = []
-
     for key in boundary:
         a = cablenet.vertex_coordinates(key)
         r = cablenet.residual(key)
         b = add_vectors(a, r)
-        pt = intersection_line_plane((a, b), plane)
-
+        pt = intersection_line_plane((a, b), (frame.point, frame.zaxis))
         intersections.append(pt)
 
-    # ==============================================================================
-    # Compute support beams
-    # ==============================================================================
+    return intersections
 
+
+def build_meshes(boxes, plane):
+    beams = [extrude_normal(box, BEAM_WIDTH, plane) for box in boxes]
+    faces = [[0, 1, 2, 3], [4, 5, 6, 7], [0, 1, 5, 4], [1, 2, 6, 5], [2, 3, 7, 6], [3, 0, 4, 7]]
+    meshes = [Mesh.from_vertices_and_faces(points, faces) for points in beams]
+    return meshes
+
+
+def compute_edge(orientation):
+    # ==============================================================================
+    # Evaluate geometry
+    # ==============================================================================
+    boundary = list(cablenet.vertices_where({'constraint': orientation}))
+    ordered = list(cablenet.vertices_on_boundary(ordered=True))
+    boundary[:] = [key for key in ordered if key in boundary]
+
+    frames = get_boundary_plane(boundary)
+    intersections = intersect_residual(boundary, frames[1])
     boxes = []
+    # ==============================================================================
+    # Fit support beams
+    # ==============================================================================
     points = intersections
-    max_size = (1.2, 0.2)
-    beam_thickness = 0.04
-
     while True:
         end = len(points)
         if end <= 2:
@@ -127,62 +126,55 @@ def compute_edge(orientation):
             pts = transform_points(pts, xform)
             bbox = bounding_box_xy(pts)
             bbox = offset_polygon(bbox, -PADDING)
-            fit = check_size(bbox, max_size[0], max_size[1])
+            fit = check_size(bbox, MAX_SIZE[0], MAX_SIZE[1])
             if fit:
                 bbox = transform_points(bbox, xform.inverse())
-                box = extrude_normal(bbox, beam_thickness, plane)
-                boxes.append(box)
+                boxes.append(bbox)
                 points = points[end - (index + 1):]
                 break
     # ==============================================================================
-    # Boxes to meshes
+    # Build beams meshes
     # ==============================================================================
+    meshes = build_meshes(boxes, frames[0])
+    return {'meshes' : meshes, 'frames' : frames, 'intersections' : intersections, 'layer' : str(orientation)}
 
-    faces = [[0, 1, 2, 3], [4, 5, 6, 7], [0, 1, 5, 4], [1, 2, 6, 5], [2, 3, 7, 6], [3, 0, 4, 7]]
-    meshes = [Mesh.from_vertices_and_faces(points, faces) for points in boxes]
 
+def draw_edge_rhino(edge):
     # ==============================================================================
     # Use a frame artist to visualize the boundary frame.
     # ==============================================================================
-
-    artist = FrameArtist(frame, layer=str(orientation) + "::Frame", scale=0.3)
+    artist = FrameArtist(edge['frames'][0], layer=edge['layer'] + "::Frame", scale=0.3)
     artist.clear_layer()
     artist.draw()
-
-    # ==============================================================================
-    # Use a point artist to visualize the intersection points.
-    # ==============================================================================
-
-    PointArtist.draw_collection(intersections, layer=str(orientation) + "::Intersections", clear=True)
-
     # ==============================================================================
     # Use a frame artist to visualize the frame of the intersection points.
     # ==============================================================================
-
-    artist = FrameArtist(frame1, layer=str(orientation) + "::Frame", scale=0.3)
+    artist = FrameArtist(edge['frames'][1], layer=edge['layer'] + "::Frame", scale=0.3)
     artist.clear_layer()
     artist.draw()
-
+    # ==============================================================================
+    # Use a point artist to visualize the intersection points.
+    # ==============================================================================
+    PointArtist.draw_collection(edge['intersections'], layer=edge['layer'] + "::Intersections", clear=True)
     # ==============================================================================
     # Use a mesh artist to visualize beams
     # ==============================================================================
-    artist = MeshArtist(None, layer=str(orientation) + "::Bbox")
+    artist = MeshArtist(None, layer=edge['layer'] + "::BBox")
     artist.clear_layer()
-
-    for mesh in meshes:
-        artist = MeshArtist(mesh, layer=str(orientation) + "::Bbox")
+    for mesh in edge['meshes']:
+        artist = MeshArtist(mesh, layer=edge['layer'] + "::BBox")
         artist.draw_mesh()
 
-# ==============================================================================
-# Make a cablenet.
-# ==============================================================================
 
+# ==============================================================================
+# Main
+# ==============================================================================
 
 HERE = os.path.dirname(__file__)
 FILE_I = os.path.join(HERE, 'data', 'cablenet.json')
 
 cablenet = Cablenet.from_json(FILE_I)
-# cablenet = Cablenet.from_json(FILE_I)
+# cablenet: Cablenet = Cablenet.from_json(FILE_I)
 
 # ==============================================================================
 # Parameters
@@ -190,119 +182,19 @@ cablenet = Cablenet.from_json(FILE_I)
 
 OFFSET = 0.200
 PADDING = 0.020
+BEAM_WIDTH = 0.040
+MAX_SIZE = (1.20, 0.20)
 
 # ==============================================================================
 # Run
 # ==============================================================================
 
-compute_edge('SOUTH')
-compute_edge('NORTH')
+south = compute_edge('SOUTH')
+north = compute_edge('NORTH')
 
+# ==============================================================================
+# Draw rhino geometry
+# ==============================================================================
 
-# SOUTH = list(cablenet.vertices_where({'constraint': 'SOUTH'}))
-# boundary = list(cablenet.vertices_on_boundary(ordered=True))
-# SOUTH[:] = [key for key in boundary if key in SOUTH]
-#
-# # ==============================================================================
-# # Boundary plane
-# # ==============================================================================
-#
-# a = cablenet.vertex_coordinates(SOUTH[0])
-# b = cablenet.vertex_coordinates(SOUTH[-1])
-#
-# xaxis = subtract_vectors(b, a)
-# yaxis = [0, 0, 1.0]
-# zaxis = cross_vectors(xaxis, yaxis)
-#
-# xaxis = cross_vectors(yaxis, zaxis)
-#
-# frame = Frame(a, xaxis, yaxis)
-#
-# point = add_vectors(frame.point, scale_vector(frame.zaxis, OFFSET))
-# normal = frame.zaxis
-# plane = point, normal
-#
-# # ==============================================================================
-# # Intersections
-# # ==============================================================================
-#
-# intersections = []
-#
-# for key in SOUTH:
-#     a = cablenet.vertex_coordinates(key)
-#     r = cablenet.residual(key)
-#     b = add_vectors(a, r)
-#     pt = intersection_line_plane((a, b), plane)
-#
-#     intersections.append(pt)
-#
-# # ==============================================================================
-# # Compute support beams
-# # ==============================================================================
-#
-# boxes = []
-# points = intersections
-# max_size = (1.2, 0.2)
-# beam_thickness = 0.04
-#
-# while True:
-#     end = len(points)
-#     if end <= 2:
-#         break
-#     for index in range(0, end):
-#         pts = points[0: end - index]
-#         if len(pts) < 3:
-#             continue
-#         bbox = pca_numpy(pts)
-#         frame1 = Frame(bbox[0], bbox[1][0], bbox[1][1])
-#         xform = Transformation.from_frame_to_frame(frame1, Frame.worldXY())
-#         pts = transform_points(pts, xform)
-#         bbox = bounding_box_xy(pts)
-#         bbox = offset_polygon(bbox, -PADDING)
-#         fit = check_size(bbox, max_size[0], max_size[1])
-#         if fit:
-#             bbox = transform_points(bbox, xform.inverse())
-#             box = extrude_normal(bbox, beam_thickness, plane)
-#             boxes.append(box)
-#             points = points[end - (index + 1):]
-#             break
-#
-#
-# # ==============================================================================
-# # Boxes to meshes
-# # ==============================================================================
-#
-# faces = [[0, 1, 2, 3], [4, 5, 6, 7], [0, 1, 5, 4], [1, 2, 6, 5], [2, 3, 7, 6], [3, 0, 4, 7]]
-# meshes = [Mesh.from_vertices_and_faces(points, faces) for points in boxes]
-#
-# # ==============================================================================
-# # Use a frame artist to visualize the boundary frame.
-# # ==============================================================================
-#
-# artist = FrameArtist(frame, layer="SOUTH::Frame", scale=0.3)
-# artist.clear_layer()
-# artist.draw()
-#
-# # ==============================================================================
-# # Use a point artist to visualize the intersection points.
-# # ==============================================================================
-#
-# PointArtist.draw_collection(intersections, layer="SOUTH::Intersections", clear=True)
-#
-# # ==============================================================================
-# # Use a frame artist to visualize the frame of the intersection points.
-# # ==============================================================================
-#
-# artist = FrameArtist(frame1, layer="SOUTH::Frame", scale=0.3)
-# artist.clear_layer()
-# artist.draw()
-#
-# # ==============================================================================
-# # Use a mesh artist to visualize beams
-# # ==============================================================================
-# artist = MeshArtist(None, layer="SOUTH::Bbox")
-# artist.clear_layer()
-#
-# for mesh in meshes:
-#     artist = MeshArtist(mesh, layer="SOUTH::Bbox")
-#     artist.draw_mesh()
+draw_edge_rhino(south)
+draw_edge_rhino(north)
